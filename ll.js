491597054d52e28920c9d00ef3b1e607f1a3b7fa
Saransh019 <b20132@students.iitmandi.ac.in>
2022-02-26 21:59:39 +0530

    Stack done

--- ll.js
+++ ll.js
@@ -1,3 +1,4 @@
+
 function matteKudasai() {
     var millisec=400;
     return new Promise(function(resolve) {
@@ -7,11 +8,13 @@ function matteKudasai() {
     });
 }
 
-async function addd(){
-    const list = document.getElementsByClassName("box");
+async function pushh(){
+    var list = document.getElementsByClassName("box");
     var val = document.getElementById("addval").value;
-    var newnode  = document.createElement("div")
-    newnode.innerHTML = val
+    var head = document.getElementById("headpic");
+    var newnode  = document.createElement("div");
+    head.style.display="block"; 
+    newnode.innerHTML = val;
     newnode.classList.add("box");
     newnode.classList.add("flex-item");
     newnode.classList.add("node");
@@ -21,23 +24,41 @@ async function addd(){
     if(list.length!=0){
         list[list.length-1].style.background = "green";
         await matteKudasai();
-        list[list.length-1].style.background = "black";
+        list[list.length-1].style.background = "cyan";
         await matteKudasai();
     }
     if(list.length!=0)
         document.querySelector("#list").appendChild(p1);
     document.querySelector("#list").appendChild(newnode);
-    docu
+    var list = document.getElementsByClassName("box");
+    if(list.length>0){
+    var off = list[list.length-1]
+    head.style.left = off.offsetLeft-25 + "px";
+    head.style.top = off.offsetTop-180+"px";
+    }
 }
 
 async function popp(){
-    const list = document.getElementsByClassName("box");
-        list[list.length-1].style.background = "green";
+    var list = document.getElementsByClassName("box");
+    var head = document.getElementById("headpic");
+    if(list.length==1){
+        head.style.display = "none";
+    }
+    if(list.length>0){
+        list[list.length-1].style.background = "red";
         await matteKudasai();
-        list[list.length-1].style.background = "black";
+        list[list.length-1].style.background = "cyan";
         await matteKudasai();    
-    var boxx = document.querySelector(".box:last-child");
-    boxx.remove();
-    var imgg = document.querySelector("img:last-child");
-    imgg.remove();
+        var boxx = document.querySelector(".box:last-child");
+        boxx.remove();
+        var imgg = document.querySelector("img:last-child");
+        imgg.remove();
+        var off = list[list.length-1];
+        head.style.left = off.offsetLeft-25 + "px";
+        head.style.top = off.offsetTop-180+"px";
+
+    }
+    else{
+        head.style.display = "none";
+    }
 }
