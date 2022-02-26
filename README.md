2d749723dec025a9e0b5877c4d7b5b3ca2ea9a13
Ritam <b20127@students.iitmandi.ac.in>
2022-02-26 15:35:56 +0530

    Added more functionality to the array generator and added selection sort

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This is a webapp which helps visualize various algorithms such as sorting algorithms, graphing algorithms and various Data Structures.
 
+Install live server in vs code and then open the port specified in liveserver. Open up the port in browser and you will be good to go.
--- index.js
+++ index.js
@@ -1,45 +1,77 @@
-function genBars() {
+function genBars(len) {
     //var l = document.querySelector("#arrayLength").value();
 
-    arr = []
-    for (var i = 0; i < 50; i++) {
-        var n = Math.floor(Math.random() * 100) + 1;
+    arr = [];
+    for (var i = 0; i < len; i++) {
+        var n = Math.floor(Math.random() * 200) + 1;
         arr.push(n);
     }
     var bars = document.querySelector("#bars");
     for (let i = 0; i < arr.length; i++) {
         const bar = document.createElement("div");
-        bar.style.height = `${arr[i]*2}px`;
-        bar.classList.add('bar');
-        bar.classList.add('flex-item');
+        bar.style.height = `${arr[i] * 2}px`;
+        bar.classList.add("bar");
+        bar.classList.add("flex-item");
         bars.appendChild(bar);
     }
 }
 
-function insertionSort() {
+function arrnew() {
+    var l = document.getElementById("arr_sz").value;
+    console.log(l);
+    var bars = document.getElementById("bars");
+    bars.remove();
+    var bb = document.createElement("div");
+    bb.setAttribute("id", "bars");
+    document.querySelector('body').appendChild(bb);
+    // console.log(document.getElementById("bars"))/
+    genBars(l);
+}
+
+async function insertionSort() {
     let bars = document.querySelectorAll(".bar");
     var len = bars.length;
-    for (let i = 1; i < len; i += 1) {
+    for (let i = 0; i < len; i += 1) {
         let j = i;
-        while (j > 0 && parseInt(bars[j].style.height) < parseInt(bars[j - 1].style.height)) {
+        while (
+            j > 0 &&
+            parseInt(bars[j].style.height) < parseInt(bars[j - 1].style.height)
+        ) {
             var temp = bars[j].style.height;
             bars[j].style.height = bars[j - 1].style.height;
             bars[j - 1].style.height = temp;
+            bars[j].style.background = "red";
+            bars[j - 1].style.background = "red";
+            await matteKudasai();
+            bars[j].style.background = "cyan";
+            bars[j - 1].style.background = "cyan";
             j--;
         }
     }
+    for (var i = 0; i < bars.length; i++) {
+        bars[i].style.background = "green";
+        await matteKudasai();
+    }
 }
 
-function swap(a, b) {
-    return [b, a];
+function matteKudasai() {
+    var millisec;
+    if (document.getElementById("speed").value == 'slow') millisec = 100;
+    if (document.getElementById("speed").value == 'medium') millisec = 50;
+    if (document.getElementById("speed").value == 'high') millisec = 25;
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve("");
+        }, millisec);
+    });
 }
 
-console.log(swap(1, 2));
-
-function bubbleSort() {
+async function bubbleSort() {
     var bars = document.querySelectorAll(".bar");
     for (var i = 0; i < bars.length; i++) {
         for (var j = 0; j < bars.length - i - 1; j++) {
+            bars[j].style.background = "red";
+            bars[j + 1].style.background = "red";
             if (parseInt(bars[j].style.height) > parseInt(bars[j + 1].style.height)) {
                 console.log(bars[j], bars[j + 1]);
                 var temp = bars[j].style.height;
@@ -47,19 +79,47 @@ function bubbleSort() {
                 bars[j + 1].style.height = temp;
                 console.log(bars[j], bars[j + 1]);
             }
+            await matteKudasai();
+            bars[j].style.background = "cyan";
+            bars[j + 1].style.background = "cyan";
+        }
+        for (var j = bars.length - i - 1; j < bars.length; j++) {
+            bars[j].style.background = "blue";
         }
     }
     for (var i = 0; i < bars.length; i++) {
-        console.log(bars[i].style.height);
+        bars[i].style.background = "green";
+        await matteKudasai();
     }
 }
 
+async function selectionSort() {
+    var bars = document.querySelectorAll(".bar");
+    for (var i = 0; i < bars.length; i++) {
+        var mi = 40000,
+            ind = i;
+        for (var j = i; j < bars.length; j++) {
+            if (mi > parseInt(bars[j].style.height)) {
+                mi = parseInt(bars[j].style.height);
+                ind = j;
+            }
+        }
+        var temp = bars[ind].style.height;
+        bars[ind].style.height = bars[i].style.height;
+        bars[i].style.height = temp;
+        bars[ind].style.background = "red";
+        bars[i].style.background = "red";
+        await matteKudasai();
+        bars[i].style.background = "blue";
+        if (ind != i) {
+            bars[ind].style.background = "cyan";
+        }
+    }
+    for (var i = 0; i < bars.length; i++) {
+        bars[i].style.background = "green";
+        await matteKudasai();
+    }
+}
 
-// function mergeSort()
-// {
-
-// }
-
-genBars();
-insertionSort();
+genBars(50);
 //insertionSort();
