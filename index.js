7034ab96513374f28dbb054f9a3bac84f5ecb9bc
Yash <b20241@students.iitmandi.ac.in>
2022-02-26 15:37:30 +0530

    Mereg Sort added

--- index.js
+++ index.js
@@ -1,125 +1,162 @@
 function genBars(len) {
-    //var l = document.querySelector("#arrayLength").value();
+  //var l = document.querySelector("#arrayLength").value();
 
-    arr = [];
-    for (var i = 0; i < len; i++) {
-        var n = Math.floor(Math.random() * 200) + 1;
-        arr.push(n);
-    }
-    var bars = document.querySelector("#bars");
-    for (let i = 0; i < arr.length; i++) {
-        const bar = document.createElement("div");
-        bar.style.height = `${arr[i] * 2}px`;
-        bar.classList.add("bar");
-        bar.classList.add("flex-item");
-        bars.appendChild(bar);
-    }
+  arr = [];
+  for (var i = 0; i < len; i++) {
+    var n = Math.floor(Math.random() * 200) + 1;
+    arr.push(n);
+  }
+  var bars = document.querySelector("#bars");
+  for (let i = 0; i < arr.length; i++) {
+    const bar = document.createElement("div");
+    bar.style.height = `${arr[i] * 2}px`;
+    bar.classList.add("bar");
+    bar.classList.add("flex-item");
+    bars.appendChild(bar);
+  }
 }
 
 function arrnew() {
-    var l = document.getElementById("arr_sz").value;
-    console.log(l);
-    var bars = document.getElementById("bars");
-    bars.remove();
-    var bb = document.createElement("div");
-    bb.setAttribute("id", "bars");
-    document.querySelector('body').appendChild(bb);
-    // console.log(document.getElementById("bars"))/
-    genBars(l);
+  var l = document.getElementById("arr_sz").value;
+  console.log(l);
+  var bars = document.getElementById("bars");
+  bars.remove();
+  var bb = document.createElement("div");
+  bb.setAttribute("id", "bars");
+  document.querySelector("body").appendChild(bb);
+  // console.log(document.getElementById("bars"))/
+  genBars(l);
 }
 
 async function insertionSort() {
-    let bars = document.querySelectorAll(".bar");
-    var len = bars.length;
-    for (let i = 0; i < len; i += 1) {
-        let j = i;
-        while (
-            j > 0 &&
-            parseInt(bars[j].style.height) < parseInt(bars[j - 1].style.height)
-        ) {
-            var temp = bars[j].style.height;
-            bars[j].style.height = bars[j - 1].style.height;
-            bars[j - 1].style.height = temp;
-            bars[j].style.background = "red";
-            bars[j - 1].style.background = "red";
-            await matteKudasai();
-            bars[j].style.background = "cyan";
-            bars[j - 1].style.background = "cyan";
-            j--;
-        }
+  let bars = document.querySelectorAll(".bar");
+  var len = bars.length;
+  for (let i = 0; i < len; i += 1) {
+    let j = i;
+    while (
+      j > 0 &&
+      parseInt(bars[j].style.height) < parseInt(bars[j - 1].style.height)
+    ) {
+      var temp = bars[j].style.height;
+      bars[j].style.height = bars[j - 1].style.height;
+      bars[j - 1].style.height = temp;
+      bars[j].style.background = "red";
+      bars[j - 1].style.background = "red";
+      await matteKudasai();
+      bars[j].style.background = "cyan";
+      bars[j - 1].style.background = "cyan";
+      j--;
     }
-    for (var i = 0; i < bars.length; i++) {
-        bars[i].style.background = "green";
-        await matteKudasai();
-    }
+  }
+  for (var i = 0; i < bars.length; i++) {
+    bars[i].style.background = "green";
+    await matteKudasai();
+  }
 }
 
 function matteKudasai() {
-    var millisec;
-    if (document.getElementById("speed").value == 'slow') millisec = 100;
-    if (document.getElementById("speed").value == 'medium') millisec = 50;
-    if (document.getElementById("speed").value == 'high') millisec = 25;
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            resolve("");
-        }, millisec);
-    });
+  var millisec;
+  if (document.getElementById("speed").value == "slow") millisec = 100;
+  if (document.getElementById("speed").value == "medium") millisec = 50;
+  if (document.getElementById("speed").value == "high") millisec = 25;
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("");
+    }, millisec);
+  });
 }
 
 async function bubbleSort() {
-    var bars = document.querySelectorAll(".bar");
-    for (var i = 0; i < bars.length; i++) {
-        for (var j = 0; j < bars.length - i - 1; j++) {
-            bars[j].style.background = "red";
-            bars[j + 1].style.background = "red";
-            if (parseInt(bars[j].style.height) > parseInt(bars[j + 1].style.height)) {
-                console.log(bars[j], bars[j + 1]);
-                var temp = bars[j].style.height;
-                bars[j].style.height = bars[j + 1].style.height;
-                bars[j + 1].style.height = temp;
-                console.log(bars[j], bars[j + 1]);
-            }
-            await matteKudasai();
-            bars[j].style.background = "cyan";
-            bars[j + 1].style.background = "cyan";
-        }
-        for (var j = bars.length - i - 1; j < bars.length; j++) {
-            bars[j].style.background = "blue";
-        }
+  var bars = document.querySelectorAll(".bar");
+  for (var i = 0; i < bars.length; i++) {
+    for (var j = 0; j < bars.length - i - 1; j++) {
+      bars[j].style.background = "red";
+      bars[j + 1].style.background = "red";
+      if (parseInt(bars[j].style.height) > parseInt(bars[j + 1].style.height)) {
+        console.log(bars[j], bars[j + 1]);
+        var temp = bars[j].style.height;
+        bars[j].style.height = bars[j + 1].style.height;
+        bars[j + 1].style.height = temp;
+        console.log(bars[j], bars[j + 1]);
+      }
+      await matteKudasai();
+      bars[j].style.background = "cyan";
+      bars[j + 1].style.background = "cyan";
     }
-    for (var i = 0; i < bars.length; i++) {
-        bars[i].style.background = "green";
-        await matteKudasai();
+    for (var j = bars.length - i - 1; j < bars.length; j++) {
+      bars[j].style.background = "blue";
     }
+  }
+  for (var i = 0; i < bars.length; i++) {
+    bars[i].style.background = "green";
+    await matteKudasai();
+  }
 }
 
 async function selectionSort() {
-    var bars = document.querySelectorAll(".bar");
-    for (var i = 0; i < bars.length; i++) {
-        var mi = 40000,
-            ind = i;
-        for (var j = i; j < bars.length; j++) {
-            if (mi > parseInt(bars[j].style.height)) {
-                mi = parseInt(bars[j].style.height);
-                ind = j;
-            }
-        }
-        var temp = bars[ind].style.height;
-        bars[ind].style.height = bars[i].style.height;
-        bars[i].style.height = temp;
-        bars[ind].style.background = "red";
-        bars[i].style.background = "red";
-        await matteKudasai();
-        bars[i].style.background = "blue";
-        if (ind != i) {
-            bars[ind].style.background = "cyan";
-        }
+  var bars = document.querySelectorAll(".bar");
+  for (var i = 0; i < bars.length; i++) {
+    var mi = 40000,
+      ind = i;
+    for (var j = i; j < bars.length; j++) {
+      if (mi > parseInt(bars[j].style.height)) {
+        mi = parseInt(bars[j].style.height);
+        ind = j;
+      }
     }
-    for (var i = 0; i < bars.length; i++) {
-        bars[i].style.background = "green";
-        await matteKudasai();
+    var temp = bars[ind].style.height;
+    bars[ind].style.height = bars[i].style.height;
+    bars[i].style.height = temp;
+    bars[ind].style.background = "red";
+    bars[i].style.background = "red";
+    await matteKudasai();
+    bars[i].style.background = "blue";
+    if (ind != i) {
+      bars[ind].style.background = "cyan";
     }
+  }
+  for (var i = 0; i < bars.length; i++) {
+    bars[i].style.background = "green";
+    await matteKudasai();
+  }
+}
+
+async function MergeSorted(a, s, e) {
+  var mid = Math.floor((s + e) / 2);
+  var i = s;
+  var j = mid + 1;
+  var ans = [];
+  while (i <= mid && j <= e) {
+    if (parseInt(a[i].style.height) < parseInt(a[j].style.height))
+      ans.push(a[i++].style.height);
+    else ans.push(a[j++].style.height);
+  }
+  while (i <= mid) ans.push(a[i++].style.height);
+  while (j <= e) ans.push(a[j++].style.height);
+
+  // copying
+  var index = s;
+  for (var p = 0; p < ans.length; ++p) {
+    //console.log(ans[p]);
+    a[index++].style.height = ans[p];
+  }
+}
+
+async function mergeSort(a, s, e) {
+  // base case
+  if (s >= e) return;
+
+  // recusive case
+  var mid = Math.floor((s + e) / 2);
+  mergeSort(a, s, mid);
+  mergeSort(a, mid + 1, e);
+
+  MergeSorted(a, s, e);
+}
+
+async function MergeSort() {
+  var bars = document.querySelectorAll(".bar");
+  mergeSort(bars, 0, bars.length - 1);
 }
 
 genBars(50);
-//insertionSort();
